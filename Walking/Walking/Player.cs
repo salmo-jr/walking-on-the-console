afea5fa741a82273f6f53e47c4e569c11a8139ad
CAR0417140W10-1\Administrador <salmo.sjunior@sp.senac.br>
2019-03-11 21:22:40 -0300

    Initial commit

---
+++ Walking/Walking/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walking
+{
+    class Player
+    {
+        public Byte Posicao { set; get; }
+        public String Aparencia { set; get; }
+        public String FacingRight { set; get; }
+        public String FacingLeft { set; get; }
+        public Boolean Pulando { set; get; }
+
+        public Player(String right, String left)
+        {
+            Posicao = 0;
+            FacingRight = right;
+            FacingLeft = left;
+            Aparencia = FacingRight;
+            Pulando = false;
+        }
+
+        public void pula()
+        {
+            Console.Beep(234, 100);
+        }
+
+        public void anda(Boolean direcao, Byte limite)
+        {
+            if (direcao)
+            {
+                if (Posicao < limite)
+                {
+                    Posicao++;
+                    Aparencia = FacingRight;
+                }
+            }
+            else
+            {
+                if (Posicao > 0)
+                {
+                    Posicao--;
+                    Aparencia = FacingLeft;
+                }
+            }
+        }
+    }
+}
---
+++ Walking/Walking/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walking
+{
+    class Program
+    {
+        const byte limite = 60;
+        const string chao = "_";
+
+        static void Main(string[] args)
+        {
+            Player p = new Player("p", "q");
+            ConsoleKey tecla;
+
+            imprime(p);
+
+            do {
+                tecla = Console.ReadKey().Key;
+
+                switch (tecla)
+                {
+                    case ConsoleKey.LeftArrow:
+                        // ANDA PARA ESQUERDA
+                        p.anda(false, limite);
+                        imprime(p);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        // ANDA PARA DIREITA
+                        p.anda(true, limite);
+                        imprime(p);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        // PULA
+                        p.Pulando = true;
+                        imprime(p);
+                        p.pula();
+                        p.Pulando = false;
+                        imprime(p);
+                        break;
+                    default:
+                        imprime(p);
+                        break;
+                }
+            } while (tecla != ConsoleKey.Escape);
+        }
+
+        static void imprime(Player p)
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+            // IMPRIME A PRIMEIRA LINHA
+            if (p.Pulando)
+            {
+                for (Byte i = 0; i < p.Posicao; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(p.Aparencia);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            // IMPRIME SEGUNDA LINHA
+            for (Byte i = 0; i < p.Posicao; i++)
+            {
+                Console.Write(chao);
+            }
+
+            if (p.Pulando) Console.Write(chao);
+            else Console.Write(p.Aparencia);
+
+            for (Byte i = 0; i < (limite - p.Posicao); i++)
+            {
+                Console.Write(chao);
+            }
+        }
+    }
+}
