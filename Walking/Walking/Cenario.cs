7dee954690c59003e57e71be2d784d529139b961
CAR0417140W10-1\Administrador <salmo.sjunior@sp.senac.br>
2019-03-12 15:27:52 -0300

    improvement to use Cenario class

---
+++ Walking/Walking/Cenario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walking
+{
+    class Cenario
+    {
+        public String Chao { set; get; }
+        public Byte Tamanho { set; get; }
+
+        public Cenario(String chao, Byte tamanho)
+        {
+            this.Chao = chao;
+            this.Tamanho = tamanho;
+        }
+
+        public void imprime(Player p)
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+            // IMPRIME A PRIMEIRA LINHA
+            if (p.Pulando)
+            {
+                for (Byte i = 0; i < p.Posicao; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(p.Aparencia);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            // IMPRIME SEGUNDA LINHA
+            for (Byte i = 0; i < p.Posicao; i++)
+            {
+                Console.Write(Chao);
+            }
+
+            if (p.Pulando) Console.Write(Chao);
+            else Console.Write(p.Aparencia);
+
+            for (Byte i = 0; i < (Tamanho - p.Posicao); i++)
+            {
+                Console.Write(Chao);
+            }
+        }
+    }
+}
--- Walking/Walking/Program.cs
+++ Walking/Walking/Program.cs
@@ -8,15 +8,13 @@ namespace Walking
 {
     class Program
     {
-        const byte limite = 60;
-        const string chao = "_";
-
         static void Main(string[] args)
         {
             Player p = new Player("p", "q");
+            Cenario c = new Cenario("_", 60);
             ConsoleKey tecla;
 
-            imprime(p);
+            c.imprime(p);
 
             do {
                 tecla = Console.ReadKey().Key;
@@ -25,61 +23,27 @@ namespace Walking
                 {
                     case ConsoleKey.LeftArrow:
                         // ANDA PARA ESQUERDA
-                        p.anda(false, limite);
-                        imprime(p);
+                        p.anda(false, c.Tamanho);
+                        c.imprime(p);
                         break;
                     case ConsoleKey.RightArrow:
                         // ANDA PARA DIREITA
-                        p.anda(true, limite);
-                        imprime(p);
+                        p.anda(true, c.Tamanho);
+                        c.imprime(p);
                         break;
                     case ConsoleKey.UpArrow:
                         // PULA
                         p.Pulando = true;
-                        imprime(p);
+                        c.imprime(p);
                         p.pula();
                         p.Pulando = false;
-                        imprime(p);
+                        c.imprime(p);
                         break;
                     default:
-                        imprime(p);
+                        c.imprime(p);
                         break;
                 }
             } while (tecla != ConsoleKey.Escape);
-        }
-
-        static void imprime(Player p)
-        {
-            Console.Clear();
-            Console.WriteLine();
-
-            // IMPRIME A PRIMEIRA LINHA
-            if (p.Pulando)
-            {
-                for (Byte i = 0; i < p.Posicao; i++)
-                {
-                    Console.Write(" ");
-                }
-                Console.WriteLine(p.Aparencia);
-            }
-            else
-            {
-                Console.WriteLine();
-            }
-
-            // IMPRIME SEGUNDA LINHA
-            for (Byte i = 0; i < p.Posicao; i++)
-            {
-                Console.Write(chao);
-            }
-
-            if (p.Pulando) Console.Write(chao);
-            else Console.Write(p.Aparencia);
-
-            for (Byte i = 0; i < (limite - p.Posicao); i++)
-            {
-                Console.Write(chao);
-            }
         }
     }
 }
